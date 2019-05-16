663d3619ee2866f628765a900ded9868742328ad
jyang49 <jason_yang490@yahoo.com>
2019-05-15 17:48:24 -0700

    update

--- jenkins/pipeline/template.groovy
+++ jenkins/pipeline/template.groovy
@@ -1,6 +1,7 @@
 pipeline {
-  agent any 
-
+  //agent any 
+  agent {label 'agent1'}
+  
   options {
     buildDiscarder(logRotator(numToKeepStr: '12'))
     timestamps() 
@@ -9,13 +10,17 @@ pipeline {
 
   parameters {
     string(name: 'hobby', description: 'favorite hobby', defaultValue: 'reading')
+    string(name: 'mysleep', description: 'delay seconds', defaultValue: '10')
   }
 
   stages {
     stage('Build') { 
       steps {
         echo "build 1"
+        echo "my hobby: " + params.hobby
+        sh "echo my hobby with sh: ${hobby}"
         echo "build 2"
+        sh "sleep ${mysleep}"
       }
     }
     stage('Test') { 
